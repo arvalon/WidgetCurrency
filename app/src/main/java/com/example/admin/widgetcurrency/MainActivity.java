69f6912e30be5720190dd84e74c9f772dbbc7a00
Arvalon <me@arvalon.ru>
2023-02-17 13:52:49 +0300

    v. 1.2

--- app/src/main/java/com/example/admin/widgetcurrency/MainActivity.java
+++ app/src/main/java/com/example/admin/widgetcurrency/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.admin.widgetcurrency;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 import java.util.Date;
